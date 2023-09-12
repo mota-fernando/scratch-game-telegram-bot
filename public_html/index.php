ba01e51c8bebb05b91bf5a72f2ff44f896fc8b21
Fernando Mota <38009829+mota-fernando@users.noreply.github.com>
2023-09-12 07:13:27 -0300

    feat(config): Added softcoded credentials

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,25 @@
 <?php
 
 require_once dirname(__DIR__). '/vendor/autoload.php';
+require_once 'config/telegram_config.php';
 
 use App\Bot\TelegramBot;
 
-$telegram = new TelegramBot();
+$telegramBotApiKey = $config('api_key');
+$telegram = new TelegramBot($telegramBotApiKey);
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 $update = json_decode(file_get_contents('php://input'), true);
 
-if (!$update) {    
+if ($update) {    
+    $telegram->handleUserInput($update);
+    $telegram->sendMessage();
+
+} else {
     http_response_code(200);
     echo 'OK';
-
-} else 
-    $telegram->sendMessage($update);
-
+}
   
 
