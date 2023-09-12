2587ec94b7b2f01f4ff8e581a224d87369e8f68c
Fernando Mota <38009829+mota-fernando@users.noreply.github.com>
2023-09-12 06:37:11 -0300

    feat(bot): Added class for Telegram bot

---
+++ src/Bot/TelegramBot.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Bot;
+
+class TelegramBot {
+
+    private $telegram;
+    private $message;
+    private $text;
+    private $chatId;
+
+    public function __construct($botToken){
+        $this->telegram = new Api($botToken);
+    }
+
+    public function handleUserInput($update){
+        $this->message = $update['message'];
+        $this->text = $message['text'];
+        $this->chatId = $message['chat']['id'];
+
+    }
+
+    private function sendMessage(){        
+        if ($this->text === '/start') {
+            $this->telegram->sendMessage([
+                'chat_id' => $this->chatId,
+                'text' => 'Bem-vindo ao bot do Telegram!',
+            ]);
+        }
+    }
+}
