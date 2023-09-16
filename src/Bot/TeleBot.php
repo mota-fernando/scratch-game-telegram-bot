d028ca3add1d74aef7fb119a082f30d4b278ce40
Fernando Mota <38009829+mota-fernando@users.noreply.github.com>
2023-09-15 21:56:25 -0300

    feat(bot): Implementation of command methods

--- src/Bot/TeleBot.php
+++ src/Bot/TeleBot.php
@@ -1,16 +1,30 @@
 <?php
 
+
+namespace App\Bot;
+
 class TeleBot
 {
+
+    protected $telegramBotApiKey;
+    protected $telegramApiUrl;
+
+    public function __construct($botToken)
+    {
+       $this->telegramApiUrl = "https://api.telegram.org/bot$botToken/sendMessage?";
+    }
+
     public function handleUpdate($update)
     {
         if (isset($update['message']['text'])) {
-            $message = $update['message']['text'];
-            $chatId = $update['message']['chat']['id'];
+            $message = $update['message'];
+            $text = $message['text'];
+            $chatId = $message['chat']['id'];
+            $first_name = $message['chat']['first_name'];
 
-            switch ($message) {
+            switch ($text) {
                 case '/start':
-                    $this->sendWelcomeMessage($chatId);
+                    $this->sendWelcomeMessage($chatId, $first_name);
                     break;
                 case 'COMPAR RASPADINHA':
                     $this->sendComparRaspadinhaMessage($chatId);
@@ -28,5 +42,69 @@ class TeleBot
         }
     }
 
-   
+    private function sendWelcomeMessage($chatId, $first_name)
+    {
+        $replyMarkup = [
+            'keyboard' => [
+                [['text' => 'COMPAR RASPADINHA']],
+                [['text' => 'PRÊMIOS E VALORES']],
+                [['text' => 'COMO RESGATAR O PRÊMIO']]
+            ],
+            'resize_keyboard' => true,
+        ];
+
+        $queryParams = [
+            'chat_id' => $chatId,
+            'text'=> "Bem-vindo $first_name ao Bot da Raspadinha! Esolha uma opção",
+            'reply_markup' => json_encode($replyMarkup)
+        ];
+
+        $this->sendMessage($queryParams);
+
+    }
+
+    private function sendComparRaspadinhaMessage($chatId)
+    {
+        $queryParams = [
+            'chat_id' => $chatId,
+            'text' => 'Aqui você pode comparar as raspadinhas disponíveis.',
+        ];
+
+        $this->sendMessage($queryParams);
+    }
+
+    private function sendPremiosValoresMessage($chatId)
+    {
+        $queryParams = [
+            'chat_id' => $chatId,
+            'text' => 'Aqui você pode ver os prêmios e valores das raspadinhas.',
+        ];
+
+        $this->sendMessage($queryParams);
+    }
+
+    private function sendComoResgatarMessage($chatId)
+    {
+        $queryParams = [
+            'chat_id' => $chatId,
+            'text' => 'Aqui você pode aprender como resgatar seu prêmio.',
+        ];
+
+        $this->sendMessage($queryParams);
+    }
+
+    private function sendDefaultMessage($chatId)
+    {
+        $queryParams = [
+            'chat_id' => $chatId,
+            'text' => 'Desculpe, não entendi sua mensagem. Escolha uma das opções do menu.',
+        ];
+
+        $this->sendMessage($queryParams);
+    }
+
+    private function sendMessage ($queryParams)
+    {
+        $response = file_get_contents($this->telegramApiUrl . http_build_query($queryParams));
+    }
 }
